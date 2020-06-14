caa76aa92d6ea09a298fca96041929dcd3d8339c
Sasanka742 <46304880+Sasanka742@users.noreply.github.com>
2020-06-14 11:29:59 -0700

    mod0_fireworks

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -1,5 +1,8 @@
 package _00_IntroToArrays;
 
+import java.awt.Frame;
+import java.awt.Window;
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.JOptionPane;
@@ -8,23 +11,47 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	//1. make a main method
-	
+	public static void main(String[] args) {
 		//2. create an array of 5 robots.
-
+	 Robot[] robots = new Robot[5];
 		//3. use a for loop to initialize the robots.
-
+		for(int i=0;i<robots.length;i++) {
+			robots[i] = new Robot();
+			robots[i].setSpeed(10);
+		}
 			//4. make each robot start at the bottom of the screen, side by side, facing up
-	
+		int x = 5;
+		for(Robot roboPosition: robots) {
+			roboPosition.setY(550);
+			x = x+120;
+			roboPosition.setX(x);
+		}
 		//5. use another for loop to iterate through the array and make each robot move 
 	    //   a random amount less than 50.
-    	
+		for(Robot roboMove: robots) {
+			roboMove.move(new Random().nextInt(50));
+		} 
 		//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-
+		
+		while(true) {
+			for(Robot roboMove: robots) {
+				roboMove.move(new Random().nextInt(50));
+				if(roboMove.getY()<0||roboMove.getY()==0) {
+					roboMove.sparkle();
+					System.out.println("Winner: "+roboMove);
+					break;
+				}
+			}
+			
+		}	
+		
+			
+		
 		//7. declare that robot the winner and throw it a party!
     	
 		//8. try different races with different amounts of robots.
     	
 	    //9. make the robots race around a circular track.
 
-	
+	}
 }
